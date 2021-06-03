22ab43979643e3a26bdc07ed49c3d2286bd6b1d4
Krzysztof Gościński <krzysztof.goscinski@gmail.com>
2021-06-03 14:08:24 +0200

    Add lazy initialization of the global services

--- app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
@@ -1,5 +1,6 @@
 package com.techyourchance.dagger2course.common.composition
 
+import androidx.annotation.UiThread
 import com.techyourchance.dagger2course.Constants
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import com.techyourchance.dagger2course.questions.FetchQuestionsDetailsUseCase
@@ -7,15 +8,20 @@ import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread
 class AppCompositionRoot {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
 
-    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val stackoverflowApi: StackoverflowApi by lazy {
+        retrofit.create(StackoverflowApi::class.java)
+    }
 
-    public val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
-    public val fetchQuestionsDetailsUseCase get() = FetchQuestionsDetailsUseCase(stackoverflowApi)
+    val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
+    val fetchQuestionsDetailsUseCase get() = FetchQuestionsDetailsUseCase(stackoverflowApi)
 }
