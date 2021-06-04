0e7d77bd93bc7623f9e2ab4f2b0e210e57c15342
Krzysztof Gościński <krzysztof.goscinski@gmail.com>
2021-06-04 14:56:59 +0200

    Inject services from 'outside' - introducing Injector

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/fragments/BaseFragment.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/fragments/BaseFragment.kt
@@ -1,14 +1,17 @@
 package com.techyourchance.dagger2course.screens.common.fragments
 
 import androidx.fragment.app.Fragment
-import com.techyourchance.dagger2course.common.composition.PresentationCompositionRoot
+import com.techyourchance.dagger2course.common.di.Injector
+import com.techyourchance.dagger2course.common.di.PresentationCompositionRoot
 import com.techyourchance.dagger2course.screens.common.activities.BaseActivity
 
 open class BaseFragment : Fragment() {
 
-    protected val compositionRoot by lazy {
+    private val compositionRoot by lazy {
         PresentationCompositionRoot(
                 (requireActivity() as BaseActivity).activityCompositionRoot
         )
     }
+
+    protected val injector get() = Injector(compositionRoot)
 }
