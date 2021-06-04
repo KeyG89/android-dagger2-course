d7a61aa1f0cf7279aaa33956d110cbe475176b1f
Krzysztof Gościński <krzysztof.goscinski@gmail.com>
2021-06-04 13:31:10 +0200

    Context isolation

--- app/src/main/java/com/techyourchance/dagger2course/common/composition/PresentationCompositionRoot.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/composition/PresentationCompositionRoot.kt
@@ -11,6 +11,8 @@ class PresentationCompositionRoot(private val activityCompositionRoot: ActivityC
     private val fragmentManager get() = activityCompositionRoot.fragmentManager
     private val stackoverflowApi get() = activityCompositionRoot.stackoverflowApi
 
+    private val activity get() = activityCompositionRoot.activity
+
     val screensNavigator get() = activityCompositionRoot.screensNavigator
 
     val viewMvcFactory get() = ViewMvcFactory(layoutInflater)
