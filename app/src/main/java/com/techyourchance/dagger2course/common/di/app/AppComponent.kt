b0672b242346ea714dc40b61b92d50fabfd84beb
Krzysztof Gościński <krzysztof.goscinski@gmail.com>
2021-06-09 00:11:43 +0200

    Refactoring ActivityComponent to become subcomponent of AppComponent

--- app/src/main/java/com/techyourchance/dagger2course/common/di/app/AppComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/di/app/AppComponent.kt
@@ -1,13 +1,11 @@
 package com.techyourchance.dagger2course.common.di.app
 
-import android.app.Application
-import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.common.di.activity.ActivityComponent
+import com.techyourchance.dagger2course.common.di.activity.ActivityModule
 import dagger.Component
-import javax.inject.Singleton
 
 @AppScope
 @Component(modules = [AppModule::class])
 interface AppComponent {
-    fun application(): Application
-    fun stackoverflowApi(): StackoverflowApi
+    fun newActivityComponent(activityModule: ActivityModule): ActivityComponent
 }
